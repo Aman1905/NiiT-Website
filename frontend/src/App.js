dbaa6683126b0f9492dfe89e4171043f9e4b416c
Aman <amanchopra.atg@gmail.com>
2022-08-03 01:37:58 +0530

    added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Home from './pages/home/Home'
+import Courses from './pages/courses/Courses'
+import Contact from './pages/contact/Contact'
+import About from './pages/about/About'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/about' element={<About/>} />
+          <Route exact path='/courses' element={<Courses/>} />
+          <Route exact path='/contact' element={<Contact/>} />
+        </Routes>
+      </Router>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

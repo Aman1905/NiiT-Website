aac073ebd6ef3f6a2755e6eb3c3163fe2f7f790d
Aman <amanchopra.atg@gmail.com>
2022-10-18 00:02:14 +0530

    service page cmpleted

--- frontend/src/pages/services/ServiceList.jsx
+++ frontend/src/pages/services/ServiceList.jsx
@@ -7,52 +7,52 @@ const ServiceList = [
     {
         id: 2, 
         title: "Workshops",
-        desc: "HTML5, CSS3, JS, PHP, MySQL",
+        desc: "Upgrade your knowledge with live interactive Workshops/Webinars/Seminars and gain a practical experience in overall working of the industry.",
     },
     {
         id: 3, 
         title: "Tech & Non-Tech Internships",
-        desc: "Complete beginner level Python Programming",
+        desc: "Develop Professional skills and gain a corporate experience with International Internships in both technical and non technical domains.",
     },
     {
         id: 4,
         title: "Placement Assistance",
-        desc: "Artificial Intelligence using Python from A to Z",
+        desc: "Go ahead in your career by cracking your dream job with our placement assistance program. Be job-ready with professional assistance in CV/Resume building.",
     },
     {
         id: 5,
         title: "Campus Ambassador Program",
-        desc: "Master the skills of Machine Learning using Python",
+        desc: "This porgram gives an exclusive opportunity to students to become our representative in their college campus. Earn a certificate and leverage your professional network by becoming a Student Campus Ambassador.",
     },
     {
         id: 6,
         title: "Event Cordinator Program",
-        desc: "Master the skills of Data Science using Python from basics",
+        desc: "This program equips you with valuable management skills that are necessary for a rewarding career. Get ready to learn Leadership and Communication skills in an immersive corporate environment !",
     },
     {
         id: 7,
         title: "DSA Series",
-        desc: "Become a master of the AI & ML world and build exciting projects",
+        desc: "Enhance your problem solving skills with hands on mastery in Data Structures and Algorithms with our DSA series. Crack the technical round in your Interviews with proper corporate guidance from our mentors !",
     },
     {
         id: 8, 
         title: "Corporate Career Counselling",
-        desc: "Let's learn to build and deploy your 1st app into the Play Store",
+        desc: "It will help you explore your career options and guide you to choose a career that suits you. Grab the chance to know revelant Industry insights to help you towards your success.",
     },
     {
         id: 9, 
         title: "Industrial Project Training",
-        desc: "Learn the skills of Game Development and create another PUBG",
+        desc: " Industrial project working will equip you with real-time Industry experience. Learn real world project implementation with practical experience along with proper project certification.",
     },
     {
         id: 10, 
         title: "Pre-Placement Training",
-        desc: "Learn and master the basics of C language",
+        desc: "With a corporate based learning, our Pre-placement training program aims to make you placement ready. Ace your General Aptitudes Tests with proper guidance from our Industry experts.",
     },
     {
         id: 11, 
         title: "Mock interview Preparation",
-        desc: "Master the Competitve Programming using Data Structures and Algorithms ",
+        desc: "Learn how to develop your interpersonal skills with a well curated mock interview that helps you with your personality development for your interviews.",
     },
 ];
 
